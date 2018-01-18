6473e1961a3f6d6db7004656a8966e1ba1a3e3bb
ScienceYo <35572273+ScienceYo@users.noreply.github.com>
2018-01-18 11:06:02 -0600

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,4 +1,3 @@
-
 const Discord = require('discord.js');
 const client = new Discord.Client();
 
@@ -7,15 +6,10 @@ client.on('ready', () => {
 });
 
 client.on('message', message => {
-    if (message.content === ';hello') {
-    	message.reply('Hello!');
+    if (message.content === 'ping') {
+    	message.reply('pong');
   	}
 });
-
-client.on('message', message => {
-    if (message.content === ';help') {
-    	message.reply('Thank you for using Milk & Cookies Bot. The default prefix is ;. Find a list of commands on the site.');
-  	}
 
 // THIS  MUST  BE  THIS  WAY
 client.login(process.env.BOT_TOKEN);
