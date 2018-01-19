e90a083ca70c5efd6c8392dd50f3f363e9836963
ScienceYo <35572273+ScienceYo@users.noreply.github.com>
2018-01-19 07:46:53 -0600

    Update bot.js

--- bot.js
+++ bot.js
@@ -14,7 +14,7 @@ client.on('message', message => {
 
 client.on('message', message => {
     if (message.content === 'mc! help') {
-    	message.channel.send('```List of commands: \n mc! help: Displays this menu \n mc! cookie: Displays a picture of a cookie. \n mc! milk: Displays a picture of milk \n mc! version: Bot version \n mc! info: Shows information about the bot \n ```');
+    	message.channel.send('```List of commands: \n mc! help: Displays this menu \n mc! cookie: Displays a picture of a cookie. \n mc! milk: Displays a picture of milk \n mc! version: Bot version \n mc! info: Shows information about the bot ```');
   	}
 });
 
