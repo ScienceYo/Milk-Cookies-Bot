4d7aba4bdb97d01b539ccab0e274059e7ecd2e75
ScienceYo <35572273+ScienceYo@users.noreply.github.com>
2018-01-19 08:19:39 -0600

    Update bot.js

--- bot.js
+++ bot.js
@@ -6,6 +6,9 @@ client.on('ready', () => {
     
 });
 
+
+
+
 client.on('message', message => {
     if (message.content === 'mc! milk') {
     	message.channel.send('Here is a picture of some milk: https://goo.gl/upNXQ7');
